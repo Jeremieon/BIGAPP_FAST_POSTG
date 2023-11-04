a7c9af431b599fc153a8520f2ca3269b8a064f25
Jeremieon <jerry_slade@outlook.com>
2023-11-04 14:30:00 +0100

    added login

--- app/auth/router.py
+++ app/auth/router.py
@@ -7,11 +7,12 @@ from app import db
 from app.user import hashing
 from app.user.models import User
 from .jwt import create_access_token
+from .schema import Login
 router = APIRouter(tags=["authentication"])
 
-
+#OAuth2PasswordRequestForm = Depends()
 @router.post('/login')
-def login(request: OAuth2PasswordRequestForm = Depends(), database: Session = Depends(db.get_db)):
+def login(request:Login, database: Session = Depends(db.get_db)):
     user = database.query(User).filter(User.email == request.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Invalid Credentials")
--- app/domain/router.py
+++ app/domain/router.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter,status
 
 router = APIRouter(tags=['Domain'], prefix='/domain')
 
-@router.get("/domain/{domain}")
+@router.get("/{domain}")
 def get_domain(domain: str):
     response = requests.get("http://ip-api.com/json/" + domain)
     return response.json()
